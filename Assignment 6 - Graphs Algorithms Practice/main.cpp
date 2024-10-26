82ac78911fdd1c3b9691ce7683d81850dfed25c8
AThreeDimensionalLemon <139825913+AThreeDimensionalLemon@users.noreply.github.com>
2024-10-26 16:30:15 -0700

    Update main.cpp

---
+++ Assignment 6 - Graphs Algorithms Practice/main.cpp
@@ -0,0 +1,26 @@
+// 
+// Name: main.cpp
+// Author: Eisig Liang
+// Last update: 26 October 2024
+// Purpose: Implement and test Prim's and Djikstra's Algorithms
+//
+
+#include <iostream>
+#include <list>
+#include "AdjacencyList.h"
+using namespace std;
+
+void primsAlgorithm()
+{
+
+}
+
+void djikstrasAlgorithm()
+{
+
+}
+
+int main() // Test algorithms' implementations
+{
+
+}
